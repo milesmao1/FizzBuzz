a82d8e93bb959513ccde1de2eb818b8be92cce52
Miles Mao <mrmao1@gmail.com>
2023-01-29 09:25:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(var i = 0; i <= 100; i++) 
+            {
+                Console.WriteLine($"{i}:{FizzBuzz(i)}");
+            } 
+        }
+
+        public static string FizzBuzz(int num) 
+        {
+            var answer = string.Empty;
+
+            if (num % 3 == 0) 
+            {
+                answer += "Fizz";
+            }
+            if (num % 5 == 0)
+            {
+                answer += "Buzz";
+            }
+
+            return answer;
+
+        }   
+    }
+}
